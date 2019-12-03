a60bd188c21aa08454b57b4e949f0e7c017b9e54
Benoit Gambier <contact@nouvelle-techno.fr>
2019-12-03 22:07:07 +0100

    Mise à jour 4.3.9

--- src/Repository/MotsClesRepository.php
+++ src/Repository/MotsClesRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\MotsCles;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method MotsCles|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MotsClesRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MotsCles::class);
     }
