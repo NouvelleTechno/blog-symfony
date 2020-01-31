14b2bb439ba4ad931871b1a37318d127582f1616
Benoit Gambier <contact@nouvelle-techno.fr>
2020-01-31 13:51:30 +0100

    LiipImagine

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
